f00b25db6d86af8622bdbcbcd0fca3b3deea054c
Eric Foy <foy.8@wright.edu>
2023-08-08 16:46:49 -0400

    scan for two values in cheat engine with scan tabs

---
+++ twoval.cpp
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <process.h>
+
+int main()
+{
+    // cheat engine lua script: openProcess(1234)
+    std::cout << getpid() << std::endl;
+    unsigned int a = 45;
+    unsigned int b = 95;
+    std::cout << (void*)&a << ": " << a << std::endl;
+    std::cout << (void*)&b << ": " << b << std::endl;
+
+    std::string line;
+    std::string word;
+    int i;
+    
+    while (true) {
+        std::getline(std::cin, line);
+        //std::cout << line << std::endl;
+        std::stringstream linestream(line);
+        linestream >> word;
+
+        if (word == "a") {
+            linestream >> i;
+            a = a + i;
+            std::cout << (void*)&a << ": " << a << std::endl;
+            std::cout << (void*)&b << ": " << b << std::endl;
+        }
+        else if (word == "b") {
+            linestream >> i;
+            b = b + i;
+            std::cout << (void*)&a << ": " << a << std::endl;
+            std::cout << (void*)&b << ": " << b << std::endl;
+        }
+        else if (word == "exit") {
+            break;
+        }
+    }
+    
+
+    return 0;
+}
