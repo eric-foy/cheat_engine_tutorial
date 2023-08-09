db56e1be950578dd6b95460bdd0f3dc6da3d49ff
Eric Foy <foy.8@wright.edu>
2023-08-09 18:06:06 -0400

    used oneval with part 7 of the tutorial

--- oneval.cpp
+++ oneval.cpp
@@ -8,6 +8,9 @@
 // https://www.youtube.com/watch?v=v-5wehWM3tE
 // Cheat Engine 6.4 Tutorial Part 2: The Importance of Value Types
 
+// https://www.youtube.com/watch?v=MiCoP2MrDOU
+// Cheat Engine 6.4 Tutorial Part 7: Introduction to Pointer Scanning!
+
 int main()
 {
     // cheat engine lua script: openProcess(1234)
